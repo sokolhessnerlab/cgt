ec03e13159582bcb2f36d8c12ace07e64f6ee339
Hayley <hayleynroper@gmail.com>
2022-08-04 12:07:51 -0600

    add dollar sign and corrected rounding for money display

--- PyschopyTask/tasks_RDM_digitSpan/CGTbothTasks_lastrun.py
+++ PyschopyTask/tasks_RDM_digitSpan/CGTbothTasks_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.1),
-    on Wed Aug  3 14:43:44 2022
+    on Thu Aug  4 11:48:56 2022
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -64,7 +64,7 @@ frameTolerance = 0.001  # how close to onset before 'same' frame
 
 # --- Setup the Window ---
 win = visual.Window(
-    size=[2048, 1152], fullscr=True, screen=0, 
+    size=[2560, 1440], fullscr=True, screen=0, 
     winType='pyglet', allowStencil=False,
     monitor='testMonitor', color=[-1,-1,-1], colorSpace='rgb',
     blendMode='avg', useFBO=True, 
@@ -422,12 +422,22 @@ bestRhoContainer = []
 bestMu = ''
 bestMuContainer = []
 settingUpForPart2 = visual.TextStim(win=win, name='settingUpForPart2',
-    text='The first round of the gambling task is complete! \n\nSetting up for the last round of the gambling task.\n\nPlease wait...\n\n',
+    text='The first round of the gambling task is complete! \n\nSetting up for the last round of the gambling task.\n\nPlease wait...\n\n\n',
     font='Arial',
     pos=(0, 0), height=instructionsTextHeight, wrapWidth=wrap, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=-4.0);
+
+# --- Initialize components for Routine "loadDynamicChoiceset" ---
+text = visual.TextStim(win=win, name='text',
+    text='',
+    font='Open Sans',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+staticLoadChoiceset = clock.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='staticLoadChoiceset')
 
 # --- Initialize components for Routine "startDynamicTask" ---
 moveToRDMpart2 = visual.TextStim(win=win, name='moveToRDMpart2',
@@ -584,7 +594,7 @@ movealong = keyboard.Keyboard()
 
 # --- Initialize components for Routine "FSInstructions" ---
 FSGenInsText = visual.TextStim(win=win, name='FSGenInsText',
-    text='The practice for the forwards section of this task is up next.\n\nYou will complete two practice trials, each with a list of 3 numbers. \n\nType out your answer when "Recall" screen appears using the numbers at the top of the keyboard to type out the numbers in the order they were presented on the screen. \n\nIf you make a mistake you can use backspace to correct it.  \n\nDo not use spaces. \n\nFeedback will be provided.\n\nPress \'enter\' to begin the practice.',
+    text='The practice for the forwards section of this task is up next.\n\nYou will complete two practice trials, each with a list of 3 numbers. \n\nType out your answer when "Recall" appears on the screen using the numbers at the top of the keyboard to type out the numbers in the order they were presented on the screen. \n\nIf you make a mistake you can use backspace to correct it.  \n\nDo not use spaces. \n\nFeedback will be provided.\n\nPress \'enter\' to begin the practice.',
     font='Arial',
     pos=(0, 0), height=instructionsTextHeight, wrapWidth=wrap, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
@@ -739,7 +749,7 @@ feedback_text = visual.TextStim(win=win, name='feedback_text',
 
 # --- Initialize components for Routine "InstructionsBS" ---
 BSGenInsText = visual.TextStim(win=win, name='BSGenInsText',
-    text='The practice for the backwards section of this task is up next.\n\nYou will complete two practice trials, each with a list of 2 numbers. \n\nType out your answer when "Recall" screen appears using the numbers at the top of the keyboard to type out the numbers in the REVERSE order they were presented on the screen. \n\nFor example, if the numbers presented are 6 then 2, your response should be 26.\n\nIf you make a mistake you can use backspace to correct it.  \n\nDo not use spaces. \n\nFeedback will be provided.\n\nPress \'enter\' to begin the practice.',
+    text='The practice for the backwards section of this task is up next.\n\nYou will complete two practice trials, each with a list of 2 numbers. \n\nType out your answer when "Recall" appears on the screen using the numbers at the top of the keyboard to type out the numbers in the REVERSE order they were presented on the screen. \n\nFor example, if the numbers presented are 6 then 2, your response should be 26.\n\nIf you make a mistake you can use backspace to correct it.  \n\nDo not use spaces. \n\nFeedback will be provided.\n\nPress \'enter\' to begin the practice.',
     font='Arial',
     pos=(0, 0), height=instructionsTextHeight, wrapWidth=wrap, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
@@ -1133,13 +1143,13 @@ for thisTrial in trials:
     circleRight.setLineColor([1,1,1])
     lineLeft.setPos(targetPos)
     lossTxt.setPos(lossLoc)
-    lossTxt.setText(round(riskyLoss,2)
+    lossTxt.setText('$' + str(round(riskyLoss,2))
 
 )
     gainTxt.setPos(gainLoc)
-    gainTxt.setText(round(riskyGain,2))
+    gainTxt.setText('$' + str(round(riskyGain,2)))
     safeTxt.setPos(safeLoc)
-    safeTxt.setText(round(alternative,2))
+    safeTxt.setText('$' + str(round(alternative,2)))
     choice1.keys = []
     choice1.rt = []
     _choice1_allKeys = []
@@ -1900,15 +1910,19 @@ for thisStaticRDM in staticRDM:
         lossLoc=[.4,-.1]
         gainLoc=[.4,.1]
         safeLoc=[-.4,0]
+    
+    lossRounded = ("$ = " + str(round(riskyoption2, 2)))
+    gainRounded = ("$ = " + str(round(riskyoption1, 2)))
+    safeRounded = ("$ = " + str(round(safeoption, 2)))
     circleRightReal.setFillColor([1,1,1])
     circleRightReal.setLineColor([1,1,1])
     lineLeftReal.setPos(targetPos)
     lossTxtReal.setPos(lossLoc)
-    lossTxtReal.setText(round(riskyoption2,2))
+    lossTxtReal.setText(lossRounded)
     gainTxtReal.setPos(gainLoc)
-    gainTxtReal.setText(round(riskyoption1,2))
+    gainTxtReal.setText(gainRounded)
     safeTxtReal.setPos(safeLoc)
-    safeTxtReal.setText(round(safeoption,2))
+    safeTxtReal.setText(safeRounded)
     realChoice.keys = []
     realChoice.rt = []
     _realChoice_allKeys = []
@@ -2694,6 +2708,92 @@ thisExp.addData('bestMu.routineEndVal', bestMu)  # Save end routine value
 # the Routine "computeEstimates" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
+# --- Prepare to start Routine "loadDynamicChoiceset" ---
+continueRoutine = True
+# update component parameters for each repeat
+# keep track of which components have finished
+loadDynamicChoicesetComponents = [text, staticLoadChoiceset]
+for thisComponent in loadDynamicChoicesetComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+frameN = -1
+
+# --- Run Routine "loadDynamicChoiceset" ---
+while continueRoutine and routineTimer.getTime() < 2.0:
+    # get current time
+    t = routineTimer.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text* updates
+    if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text.frameNStart = frameN  # exact frame index
+        text.tStart = t  # local t and not account for scr refresh
+        text.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+        # add timestamp to datafile
+        thisExp.timestampOnFlip(win, 'text.started')
+        text.setAutoDraw(True)
+    if text.status == STARTED:
+        # is it time to stop? (based on global clock, using actual start)
+        if tThisFlipGlobal > text.tStartRefresh + 2-frameTolerance:
+            # keep track of stop time/frame for later
+            text.tStop = t  # not accounting for scr refresh
+            text.frameNStop = frameN  # exact frame index
+            # add timestamp to datafile
+            thisExp.timestampOnFlip(win, 'text.stopped')
+            text.setAutoDraw(False)
+    # *staticLoadChoiceset* period
+    if staticLoadChoiceset.status == NOT_STARTED and t >= 0-frameTolerance:
+        # keep track of start time/frame for later
+        staticLoadChoiceset.frameNStart = frameN  # exact frame index
+        staticLoadChoiceset.tStart = t  # local t and not account for scr refresh
+        staticLoadChoiceset.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(staticLoadChoiceset, 'tStartRefresh')  # time at next scr refresh
+        # add timestamp to datafile
+        thisExp.addData('staticLoadChoiceset.started', t)
+        staticLoadChoiceset.start(2)
+    elif staticLoadChoiceset.status == STARTED:  # one frame should pass before updating params and completing
+        # Updating other components during *staticLoadChoiceset*
+        text.setText('Setting up...')
+        # Component updates done
+        staticLoadChoiceset.complete()  # finish the static period
+        staticLoadChoiceset.tStop = staticLoadChoiceset.tStart + 2  # record stop time
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in loadDynamicChoicesetComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# --- Ending Routine "loadDynamicChoiceset" ---
+for thisComponent in loadDynamicChoicesetComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# using non-slip timing so subtract the expected duration of this Routine
+routineTimer.addTime(-2.000000)
+
 # --- Prepare to start Routine "startDynamicTask" ---
 continueRoutine = True
 # update component parameters for each repeat
@@ -2829,15 +2929,19 @@ for thisDynamicRDM in dynamicRDM:
         lossLoc=[.4,-.1]
         gainLoc=[.4,.1]
         safeLoc=[-.4,0]
+    
+    lossRounded = ("$ = " + str(round(riskyoption2, 2)))
+    gainRounded = ("$ = " + str(round(riskyoption1, 2)))
+    safeRounded = ("$ = " + str(round(safeoption, 2)))
     circleRightReal.setFillColor([1,1,1])
     circleRightReal.setLineColor([1,1,1])
     lineLeftReal.setPos(targetPos)
     lossTxtReal.setPos(lossLoc)
-    lossTxtReal.setText(round(riskyoption2,2))
+    lossTxtReal.setText(lossRounded)
     gainTxtReal.setPos(gainLoc)
-    gainTxtReal.setText(round(riskyoption1,2))
+    gainTxtReal.setText(gainRounded)
     safeTxtReal.setPos(safeLoc)
-    safeTxtReal.setText(round(safeoption,2))
+    safeTxtReal.setText(safeRounded)
     realChoice.keys = []
     realChoice.rt = []
     _realChoice_allKeys = []
