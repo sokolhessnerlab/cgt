92e66de73b43d0ce0fba7aa669be25bc6b195eb7
Peter Sokol-Hessner (he/him/his) <hessner@gmail.com>
2022-10-17 15:12:19 -0600

    Outlining next steps
    Writing down some of the coming steps (in some generality) in terms of analysis.
    
    Co-Authored-By: Anna Rini <81936265+annarini@users.noreply.github.com>

--- R/cgt_analysis_script.R
+++ R/cgt_analysis_script.R
@@ -16,3 +16,24 @@ fn = dir(pattern = glob2rx('cgt_processed*.csv'),full.names = T);
 data_dm = read.csv(fn[1]) # decision-making data
 data_wm = read.csv(fn[2]) # working memory data
 
+#### Data Quality Checks & Exclusions ####
+
+# Exclude on the basis of DM task performance
+# (using... check trials, RTs, choices)
+
+# Exclude on the basis of WM task performance
+# (using... )
+
+# Exclude on any other basis? 
+
+# >>>Create clean data frames<<<
+
+
+#### Basic Data Analysis ####
+# (Separately for DM & WM data)
+
+# Descriptives & simple averages of task performance
+
+
+#### Connecting Decision-Making & Working Memory ####
+
