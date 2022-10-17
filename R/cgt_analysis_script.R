d9139e20651b1c12641714dc12a47af69463880f
Peter Sokol-Hessner (he/him/his) <hessner@gmail.com>
2022-10-17 14:54:59 -0600

    Create cgt_analysis_script.R
    Initial commit of analysis file
    
    Co-Authored-By: Anna Rini <81936265+annarini@users.noreply.github.com>

---
+++ R/cgt_analysis_script.R
@@ -0,0 +1,18 @@
+# CGT Data Analysis Script
+#
+# Script to analyze the data collected online during Summer 2022 in the CGT
+# (Control & Gambling Task) study.
+
+rm(list=ls()); # Clear the workspace
+
+# identify the working directory paths
+main_wd = '/Volumes/shlab/Projects/CGT/';
+processeddata_wd = paste0(main_wd,'processeddata/')
+
+#### Loading Data ####
+setwd(processeddata_wd)
+fn = dir(pattern = glob2rx('cgt_processed*.csv'),full.names = T);
+
+data_dm = read.csv(fn[1]) # decision-making data
+data_wm = read.csv(fn[2]) # working memory data
+
