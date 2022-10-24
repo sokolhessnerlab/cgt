1ba9334ee33785cfd7dac9f69574f1e708826dca
Peter Sokol-Hessner <hessner@gmail.com>
2022-10-24 15:13:18 -0600

    Check trial exclusions
    Analysis of check trials for purposes of discarding (some) people.

--- R/cgt_analysis_script.R
+++ R/cgt_analysis_script.R
@@ -16,10 +16,37 @@ fn = dir(pattern = glob2rx('cgt_processed*.csv'),full.names = T);
 data_dm = read.csv(fn[1]) # decision-making data
 data_wm = read.csv(fn[2]) # working memory data
 
+number_of_subjects = length(unique(data_dm$subjectnumber));
+
 #### Data Quality Checks & Exclusions ####
 
 # Exclude on the basis of DM task performance
 # (using... check trials, RTs, choices)
+
+# Check Trials
+check_trial_failure = array(dim = c(number_of_subjects,1));
+
+for (subj in 1:number_of_subjects){
+  tmpdata = data_dm[data_dm$subjectnumber == subj,];
+  check_trial_index = which(tmpdata$ischecktrial==1); 
+  correct_answers = (0.5 * tmpdata$riskyopt1[check_trial_index] +
+    0.5 * tmpdata$riskyopt2[check_trial_index]) > tmpdata$safe[check_trial_index];
+  check_trial_failure[subj] = length(which(!tmpdata$choice[check_trial_index] == correct_answers))/length(check_trial_index);
+}
+
+check_trial_criterion = 0.2; # The maximum percent of check trials that can be missed
+# chance is 0.5, perfect is 0.0. 
+
+keep_check_trial = check_trial_failure <= check_trial_criterion;
+
+# Missed Choices
+# on the basis of is.na(data_dm$choice)
+
+# RTs
+
+
+# QUESTION: WHAT DO WE DO WITH NA TRIALS (MISSED, OR TOO FAST)? KEEP? DISCARD? 
+
 
 # Exclude on the basis of WM task performance
 # (using... )
