b9d760dc33c3932add0df1c2eb3bee69a90ed696
Hayley <arini2001@gmail.com>
2022-10-28 13:42:56 -0600

    added a section for RT exclusion
    possibly criteria to exclude RT data on

--- R/cgt_analysis_script.R
+++ R/cgt_analysis_script.R
@@ -28,30 +28,38 @@ check_trial_failure = array(dim = c(number_of_subjects,1));
 
 for (subj in 1:number_of_subjects){
   tmpdata = data_dm[data_dm$subjectnumber == subj,];
-  check_trial_index = which(tmpdata$ischecktrial==1); 
+  check_trial_index = which(tmpdata$ischecktrial==1);
   correct_answers = (0.5 * tmpdata$riskyopt1[check_trial_index] +
     0.5 * tmpdata$riskyopt2[check_trial_index]) > tmpdata$safe[check_trial_index];
   check_trial_failure[subj] = length(which(!tmpdata$choice[check_trial_index] == correct_answers))/length(check_trial_index);
 }
 
 check_trial_criterion = 0.2; # The maximum percent of check trials that can be missed
-# chance is 0.5, perfect is 0.0. 
+# chance is 0.5, perfect is 0.0.
 
 keep_check_trial = check_trial_failure <= check_trial_criterion;
 
 # Missed Choices
 # on the basis of is.na(data_dm$choice)
+#if missed >75% = exclude, for compensation, if i recall correctly, there should be 1 person who had 60%
+
 
 # RTs
+for (subj in 1:number_of_subjects){
+  tmpdata = data_dm[data_dm$subjectnumber == subj,];
+  correct_answers = (reactiontime >= 0.25) #I don't think we need a top cut off based upon distribution mainly around 1 second, anything above 0.2 seconds
+  incorrect_answers = (reactiontime == is.NaN) #if no response and not correct answers
+}
+reactiontime_criterion = (length((correct_answers)/(num_static_trials + num_dynamic_trials)) >= .80) #RT per participant must mostly be correct, so at least 80% of the RT must be greater than or equal to 250 ms.
 
-
-# QUESTION: WHAT DO WE DO WITH NA TRIALS (MISSED, OR TOO FAST)? KEEP? DISCARD? 
+keep_reactiontime == reactiontime_criterion
+# QUESTION: WHAT DO WE DO WITH NA TRIALS (MISSED, OR TOO FAST)? KEEP? DISCARD?
 
 
 # Exclude on the basis of WM task performance
 # (using... )
 
-# Exclude on any other basis? 
+# Exclude on any other basis?
 
 # >>>Create clean data frames<<<
 
